b77f4f528c085400f4f265041ed11576ce8c207e
Jean Claude <jeanyao@ymail.com>
2024-01-19 02:37:48 +0500

    Testing match

--- src/Component/Routing/Router/Factory/RouterFactory.php
+++ src/Component/Routing/Router/Factory/RouterFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Laventure\Component\Routing\Router\Factory;
@@ -17,7 +18,6 @@ use Laventure\Component\Routing\Router\RouterInterface;
 */
 class RouterFactory implements RouterFactoryInterface
 {
-
     /**
      * @param string $namespace
     */
