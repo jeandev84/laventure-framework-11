f30e0652af4cb6d89e3a31e502b06c46f2d17bf8
Jean Claude <jeanyao@ymail.com>
2024-01-22 00:20:02 +0500

    FileLocator

---
+++ src/Component/Filesystem/Filesystem.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Filesystem;
+
+/**
+ * Filesystem
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Filesystem
+*/
+class Filesystem implements FilesystemInterface
+{
+
+}
